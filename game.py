be55a3e6bcfccec4aadf50890e13701d29756e37
Jacob McAleese <jacobmcaleese@mail.weber.edu>
2025-11-05 11:41:14 -0700

    so many things

--- game.py
+++ game.py
@@ -7,12 +7,10 @@ This is a text-based adventure game where the player makes choices
 to navigate through a mysterious forest.
 """
 
-# TODO: Define a function called welcome_player() that:
-#       - Prints a welcome message
-#       - Asks the user for their name using input()
-#       - Welcomes the user using an f-string
-#       - Returns the player's name
+# TODO: Create an empty list called `inventory` at the top of your code.
+#       This will store items the player picks up during the game.
 
+inventory = []
 
 
 
@@ -29,32 +27,44 @@ def welcome_player():
     return player_name
 
 
-# TODO: Define a function called describe_area() that:
-#       - Prints a multi-line string describing the starting location of the game
-
 
 def describe_area():
     # Describe the starting area
     starting_area = """
-    You find yourself in a dark forest.
-    The sound of rustling leaves fills the air.
+    You find yourself in a dark forest...
+    You see two paths ahead:
     A faint path lies ahead, leading deeper into the unknown...
     """
     print(starting_area)
 
 
-name = welcome_player()
+def add_to_inventory(item):
+    inventory.append(item)
+    print("You picked up", item)
+
+player_name = welcome_player() # return player name
 describe_area()
 
+while True:
+    # Ask the player for their first decision
+    decision = input("\t1. Take the left path into the dark woods\n"
+                    "\t2. Take the right path to the mountain pass\n"
+                    "\t3. Stay where you are\n"
+                    "\t Type 'i' to view your inventory").lower()
 
-# Ask the player for their first decision
-decision = input("Do you wish to take the path? (yes or no): ").lower()
 
-# conditional evaluate
-# Respond based on the player's decision
-if decision == "yes": # = assignment operator == equivalent
-    print(f"Brave choice, {player_name}! You step onto the path and venture forward.")
-elif decision == "no":
-    print(player_name + ", you decide to wait. Perhaps courage will find you later.") # Concatenation example
-else:
-    print("Confused, you stand still, unsure of what to do.")
+
+    # conditional evaluate
+    # Respond based on the player's decision
+    if decision == "1": # = assignment operator == equivalent
+        print("You go into the dark woods.")
+        add_to_inventory("lantern")
+    elif decision == "2":
+        print("You go towards the mountain pass.") # Concatenation example
+        add_to_inventory("map")
+    elif decision == "3":
+        print("Confused, you stand still, unsure of what to do.")
+    elif decision == "i":
+        print(inventory)
+    else:
+        print("That is not a valid choice")
