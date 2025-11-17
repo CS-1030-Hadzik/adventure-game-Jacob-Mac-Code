d84bbeed36f76609e3f39e52d3ecf8ea295ec320
Jacob McAleese <jacobmcaleese@mail.weber.edu>
2025-11-17 11:23:05 -0700

    REF

--- Player.py
+++ Player.py
@@ -11,11 +11,3 @@ class Player():
 
 
 
-# TODO: (Optional Stretch) Add a check before certain choices
-#       - Example: If player.has_lantern is False, prevent entering a cave
-#       - Print a message like “It’s too dark to continue without a lantern.”
-
-# TODO: Update all print statements that used player_name to use player.name
-
-# TODO: Commit and push your code with a message like:
-#       REF player class added and game state flags implemented
